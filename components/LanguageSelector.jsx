71deace7befe91d9a764677c592d27c3e9a8fbd7
Alain DRILLON <contact@drillon-ala.in>
2019-02-17 11:26:28 +0100

    Add flags in language selector

--- components/LanguageSelector.jsx
+++ components/LanguageSelector.jsx
@@ -1,20 +1,24 @@
 import React, { Component } from 'react'
 import { Link } from '../utils/routes';
 import { LanguageContext } from '../utils/LanguageContext';
+import '../css/languageselector.scss';
 
 class LanguageSelector extends Component {
     render() {
         return (
             <LanguageContext.Consumer>
                 {value =>
-                        <ul>
-                            {value.alternateLanguages.map((alt) => (
+                        <ul id="languageselector" >
+                            {value.alternateLanguages.map((alt) => {
+                                return alt.params.lang == value.lang ? '' :
                                 <li key={alt.params.lang} >
                                     <Link route={alt.route} params={alt.params} >
-                                        <a>{alt.params.lang}</a>
+                                        <a>
+                                            <img className="languageselector-flag" src={"/static/langs/" + alt.params.lang + ".svg"} />
+                                        </a>
                                     </Link>
                                 </li>
-                            ))}
+                                })}
                         </ul>
                 }
             </LanguageContext.Consumer>
