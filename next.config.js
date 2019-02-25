2bea5f63329ed84ae7fee54b01c8f7eff6397ad3
Alain DRILLON <contact@drillon-ala.in>
2019-02-25 19:07:03 +0100

    Automatically build the list of URLs for static export

--- next.config.js
+++ next.config.js
@@ -1,2 +1,66 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass()
+const superagent = require('superagent');
+
+module.exports = withSass({
+    exportPathMap: async function() {
+        // dynamically generate all possibles URLs
+
+        let langs = require('./config/langs.json');
+        let config = require('./config/config.json');
+        let paths = {
+            '/': {page: '/'}
+        };
+
+        for (let lang of Object.keys(langs)) {
+            let langConfig = require('./langs/' + lang + '.json');
+
+            // language-specific home page
+            paths['/' + lang] = {
+                page: '/post', 
+                query: {
+                    lang: lang
+                }
+            };
+
+            for (let wpType of Object.keys(langConfig.posttypes)) {
+                // language-specific default posts
+                let translatedType = langConfig.posttypes[wpType];
+                paths['/' + lang + '/' + translatedType] = {
+                    page: '/post',
+                    query: {
+                        lang: lang,
+                        posttype: translatedType
+                    }
+                };
+
+                // language-specific post lists
+                if (config.defaultPosts[wpType] === undefined) {
+                    let translatedType = langConfig.posttypes[wpType];
+                    paths['/' + lang + '/' + translatedType] = {
+                        page: '/postlist',
+                        query: {
+                            lang: lang,
+                            posttype: translatedType
+                        }
+                    };
+                }
+                
+                // posts
+                // TODO: handle pagination in case lots of posts are created
+                let postList = (await superagent.get(langs[lang] + '/wp-json/wp/v2/' + wpType)).body;
+                for (let post of postList) {
+                    paths['/' + lang + '/' + translatedType + '/' + post.slug] = {
+                        page: '/post',
+                        query: {
+                            lang: lang,
+                            posttype: translatedType,
+                            slug: post.slug
+                        }
+                    };
+                }
+            };
+        };
+
+        return paths;
+    }
+});
